d7966b09db4147014d99a72a400bce7fdc5460be
gousset15u <morgane.gousset@telecomnancy.net>
2022-09-04 19:04:17 +0200

    [km] predict and fit functions -- dataset 1 pass

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -1,15 +1,20 @@
+from inspect import ArgSpec
 import numpy as np 
 import pandas as pd 
+import random as rd
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
 
 
 class KMeans:
     
-    def __init__(self):
+    def __init__(self, cluster):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
-        pass
+        self.cluster = cluster
+        self.eps = 0.001
+        self.list_z=[]
+        self.list_centroid=[]
         
     def fit(self, X):
         """
@@ -20,7 +25,25 @@ class KMeans:
                 m rows (#samples) and n columns (#features)
         """
         # TODO: Implement
-        raise NotImplementedError()
+        m,n = X.shape
+        centroids=np.empty([self.cluster,n])
+
+        rng = np.random.default_rng()
+
+        for c in range (0,self.cluster):
+            centroids[c] = rng.random([n])
+        # if self.cluster==10:
+        #     centroids[0]=[0.9,1]
+        #     centroids[1]=[0.7,1]
+        #     centroids[2]=[0.7,3]
+        #     centroids[3]=[0.9,5]
+        #     centroids[4]=[0.6,7]
+        #     centroids[5]=[0.5,7]
+        #     centroids[6]=[0.8,10]
+        #     centroids[7]=[0.2,4]
+        #     centroids[8]=[0,1]
+        #     centroids[9]=[0.1,6]
+        self.centroids = centroids
     
     def predict(self, X):
         """
@@ -38,8 +61,68 @@ class KMeans:
             there are 3 clusters, then a possible assignment
             could be: array([2, 0, 0, 1, 2, 1, 1, 0, 2, 2])
         """
-        # TODO: Implement 
-        raise NotImplementedError()
+        m,n=X.shape
+        z=np.empty(m)
+        distor = 100
+        distor_prev = 0
+
+        list_z=[]
+        list_centroid=[]
+        compt = 0
+        
+        # x0 = np.stack((X[:,0],np.zeros(m)),axis=-1)
+        # x1 = np.stack((X[:,1],np.zeros(m)),axis=-1)
+
+        while (distor > self.eps and distor_prev != distor):
+            compt+=1
+            distor_prev = distor
+            distor = euclidean_distortion(X,z.astype(int))
+
+            for i in range (0,m):
+                dist_eucl = np.ones(self.cluster)
+                # dist_eucl_x0 = np.ones(self.cluster)
+                # dist_eucl_x1 = np.ones(self.cluster)
+
+                for j in range (0,self.cluster):
+                    dist_eucl[j]=euclidean_distance(X[i,:],self.centroids[j])
+                    # dist_eucl_x0[j]=euclidean_distance(x0[i,:],self.centroids[j])
+                    # dist_eucl_x1[j]=euclidean_distance(x1[i,:],self.centroids[j])
+                # dist_eucl = euclidean_distance(X[i,:],self.centroids)
+                # dist_eucl = cross_euclidean_distance(X[i,:],self.centroids)
+
+                # if compt%3==0:
+                #     z[i] = np.argmin(dist_eucl)
+                # elif compt%2==1:
+                #     z[i] = np.argmin(dist_eucl_x0)
+                # else:
+                #     z[i] = np.argmin(dist_eucl_x1)
+                z[i] = np.argmin(dist_eucl)
+                #z[i] = argmin(X[i,:],self.centroids,self.cluster)
+
+            for j in range (0,self.cluster):
+                sum_coord =np.empty(n)
+                for i in range(m):
+                    sum_coord = np.add(sum_coord,(z[i]==j)*X[i,:])
+
+                effectif=0
+                for i in range(m):
+                    effectif+=(z[i]==j)
+
+                if effectif!=0:
+                    self.centroids[j]=sum_coord/effectif
+
+                else: 
+                    print("zero")
+                    #TODO: find better method to delete a centroid
+                    rng = np.random.default_rng()
+                    self.centroids[j] = rng.random(n)
+            list_z.append(z)
+            list_centroid.append(self.centroids)
+
+        self.list_z=list_z
+        self.list_centroid=list_centroid
+
+        return z.astype(int)
     
     def get_centroids(self):
         """
@@ -56,13 +139,39 @@ class KMeans:
             [xm_1, xm_2, ..., xm_n]
         ])
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
-    
+        return self.centroids
+
+    def get_list_centroids(self):
+        return self.list_centroid
+
+    def get_list_z(self):
+        return self.list_z
     
     
 # --- Some utility functions 
+
+def argmin(x,mu,c):
+    """
+    NOT USED - Finds the closest cluster centroid for the training example x
+        using the Enclidian distance between two set of points
+    
+    Args:
+        x (array<n>): n-length vector with datapoints 
+        mu (array<n>): n-length vector of a cluster's centroid
+        c (integer): integer of number of clusters
+    
+    Returns:
+        An integer with the closest cluster centroid
+    """
+    min=euclidean_distance(x,mu[0])
+    cent=0
+    for j in range (0,c):
+        arg=euclidean_distance(x,mu[j])
+        # TODO: why is it the same euclidian distance btw different xi values with same mu[j] ? 
+        if arg<min:
+            min=arg
+            cent=j
+    return cent
 
 
 def euclidean_distortion(X, z):
@@ -161,5 +270,4 @@ def euclidean_silhouette(X, z):
     # Smallest inter distance 
     inf_mask = np.where(z[:, None] == clusters[None], np.inf, 0)
     b = (D + inf_mask).min(axis=1)
-    
     return np.mean((b - a) / np.maximum(a, b))
---
+++ k_means/km_test.py
@@ -0,0 +1,61 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import k_means as km # <-- Your implementation
+
+sns.set_style('darkgrid')
+
+# ## Data set 1
+
+# data_1 = pd.read_csv('data_1.csv')
+# data_1.describe().T
+
+# # Visualize
+# plt.figure(figsize=(5, 5))
+# sns.scatterplot(x='x0', y='x1', data=data_1)
+# plt.xlim(0, 1); plt.ylim(0, 1)
+
+# # Fit Model 
+# X = data_1[['x0', 'x1']].to_numpy() 
+# model_1 = km.KMeans(2) # <-- Should work with default constructor  
+# model_1.fit(X)
+
+# # Compute Silhouette Score 
+# z = model_1.predict(X)
+# print(f'Silhouette Score: {km.euclidean_silhouette(X, z) :.3f}')
+# print(f'Distortion: {km.euclidean_distortion(X, z) :.3f}')
+
+# # Plot cluster assignments
+# C = model_1.get_centroids()
+# K = len(C)
+# _, ax = plt.subplots(figsize=(5, 5), dpi=100)
+# sns.scatterplot(x='x0', y='x1', hue=z, hue_order=range(K), palette='tab10', data=data_1[['x0', 'x1']], ax=ax);
+# sns.scatterplot(x=C[:,0], y=C[:,1], hue=range(K), palette='tab10', marker='*', s=250, edgecolor='black', ax=ax)
+# ax.legend().remove()
+
+## Data set 2
+
+data_2 = pd.read_csv('data_2.csv')
+data_2.describe().T
+
+plt.figure(figsize=(5, 5))
+sns.scatterplot(x='x0', y='x1', data=data_2);
+
+# Fit Model 
+X = data_2[['x0', 'x1']].to_numpy()
+model_2 = km.KMeans(10)  # <-- Feel free to add hyperparameters 
+model_2.fit(X)
+
+# Compute Silhouette Score 
+z = model_2.predict(X)
+print(f'Distortion: {km.euclidean_distortion(X, z) :.3f}')
+print(f'Silhouette Score: {km.euclidean_silhouette(X, z) :.3f}')
+
+# Plot cluster assignments
+C = model_2.get_centroids()
+K = len(C)
+_, ax = plt.subplots(figsize=(5, 5), dpi=100)
+sns.scatterplot(x='x0', y='x1', hue=z, hue_order=range(K), palette='tab10', data=data_2[['x0', 'x1']], ax=ax);
+sns.scatterplot(x=C[:,0], y=C[:,1], hue=range(K), palette='tab10', marker='*', s=250, edgecolor='black', ax=ax)
+ax.legend().remove()
